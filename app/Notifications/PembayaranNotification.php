1e11ffd30ed29ad2b8c743c97aac9b62a0fd5835
Umar Ulkhak <alkhak24@gmail.com>
2025-10-02 14:03:38 +0700

    feat(notification): tambahkan URL pada notifikasi pembayaran
    Menambahkan field 'url' pada payload notifikasi database pembayaran.
    URL mengarah langsung ke halaman detail pembayaran sehingga operator
    dapat membuka data lebih cepat dari notifikasi.

--- app/Notifications/PembayaranNotification.php
+++ app/Notifications/PembayaranNotification.php
@@ -61,6 +61,7 @@ class PembayaranNotification extends Notification
             'pembayaran_id' => $this->pembayaran->id,
             'title'         => 'Pembayaran Tagihan',
             'messages'      => "{$this->pembayaran->wali->name} telah melakukan pembayaran tagihan.",
+            'url' => route('pembayaran.show', $this->pembayaran->id),
         ];
     }
 }
